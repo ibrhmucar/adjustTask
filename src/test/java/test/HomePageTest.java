d96971f3d49387594d745c52c880f221d251b713
ibrahimucar <ibrhmucar@gmail.com>
2022-10-27 18:52:13 +0300

    added some new code by iucar

--- src/test/java/test/HomePageTest.java
+++ src/test/java/test/HomePageTest.java
@@ -14,19 +14,25 @@ public class HomePageTest extends TestBase {
    SearchPage srcpg = new SearchPage();
 
    public void adjustTask () {
+      // 1- Navigate to URL and verify it.
+      String expectedURL="https://en.wikipedia.org/w/index.php?search";
+      String actualURL = Driver.get().getCurrentUrl();
+      Assert.assertTrue(expectedURL.equals(actualURL));
 
+      // 2- Write "SDET" to the main search bar and search. Verify that the word that "SDET" appears in searching result.
       srcpg.searchbar.sendKeys("SDET");
       srcpg.searchButton.click();
       String expectedWord = "SDET";
       String actualWord = Driver.get().findElement(By.xpath(srcpg.searchContent)).getText();
       Assert.assertTrue(expectedWord.equals(actualWord));
 
-
+      // 3- Write "Apple Company" to the "Search Wikipedia" box and search. Verify that the word appears on the new results page.
       srcpg.quickSearchBar.sendKeys("Apple Company");
       srcpg.getQuickSearchButtom.click();
       String expectedPageContent = "Apple Inc.";
       String actualPageContent = Driver.get().findElement(By.xpath(srcpg.pageContent)).getText();
       Assert.assertTrue(expectedPageContent.equals(actualPageContent));
+
    }
 
 
